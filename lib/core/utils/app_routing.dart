923c82690772ca77662471f223945b9529f0687c
yasmeenaboghazala <yasmeenaboghazala47@gmail.com>
2023-12-08 00:26:00 +0200

    Profile_Screen

--- lib/core/utils/app_routing.dart
+++ lib/core/utils/app_routing.dart
@@ -1,36 +1,37 @@
-import 'package:flutter_grad_project/features/Blood_Bank/BloodBank/screens/Bloodd_bank/presentation/screen/blood_bank.dart';
+//import 'package:flutter_grad_project/features/Blood_Bank/BloodBank/presentation/Screens/presentation/screen/blood_bank.dart';
+import 'package:flutter_grad_project/features/profilee/presentation/screen/presentation/screen/profile_screen.dart';
 import 'package:go_router/go_router.dart';
 
-import '../../features/Blood_Bank/inquiry/presentation/screen/inquiry.dart';
-import '../../features/Blood_Bank/otp/presentation/screen/otp.dart';
-import '../../features/profilee/screens/Profile/presentation/screen/profile_screen.dart';
+//import '../../features/Blood_Bank/inquiry/presentation/screen/inquiry.dart';
+//import '../../features/Blood_Bank/otp/presentation/screen/otp.dart';
 
 abstract class AppRouters {
 // create  route path variables must start with '/......'
-  static const kdefultRoute = '/';
+  static const kdefultRoute = '/defaultRoute';
   static const kBloodBankRoute = '/BloodBank';
   static const kinquiryRoute = '/quiry';
   static const kOtpRoute = '/Otp';
   static const kProfileScreenRoute = '/ProfileScreen';
 
   static final GoRouter router = GoRouter(
+    initialLocation: kProfileScreenRoute,
     routes: [
-      GoRoute(
-        path: kBloodBankRoute,
-        builder: (context, state) => Blood_Bank(),
-      ),
-      GoRoute(
-        path: kinquiryRoute,
-        builder: (context, state) => Inguiry_Screen(),
-      ),
-      GoRoute(
-        path: kOtpRoute,
-        builder: (context, state) => Otp(),
-      ),
+      // GoRoute(
+      //   path: kBloodBankRoute,
+      //   builder: (context, state) => Blood_Bank(),
+      // ),
+      // GoRoute(
+      //   path: kOtpRoute,
+      //   builder: (context, state) => Otp(),
+      // ),
       GoRoute(
         path: kProfileScreenRoute,
         builder: (context, state) => ProfileScreen(),
       ),
+      // GoRoute(
+      //   path: kinquiryRoute,
+      //   builder: (context, state) => Inguiry_Screen(),
+      // ),
     ],
   );
 }
