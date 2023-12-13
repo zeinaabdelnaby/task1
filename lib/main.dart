078564f8521c4b549af8f4b9a832447e8a555aa9
zeinaabdelnaby <zeinaa632@gmail.com>
2023-12-13 21:30:35 +0200

    edit main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_grad_project/core/utils/app_routing.dart';
-import 'package:flutter_grad_project/features/onboarding/presentation/screen/onboarding_screen.dart';
-import 'package:flutter_grad_project/features/onboarding/presentation/widgets/onboarding1.dart';
-import 'package:flutter_grad_project/features/onboarding/presentation/widgets/onboarding2.dart';
-import 'package:flutter_grad_project/features/onboarding/presentation/widgets/onboarding3.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
@@ -16,21 +12,17 @@ class HealthCareConnectionApp extends StatelessWidget {
   // This widget is the root of our application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Onboarding(),
-    );
-  //   ScreenUtilInit(
-  //     designSize: const Size(375, 812),
-  //     minTextAdapt: true,
-  //     splitScreenMode: true,
-  //     builder: (_ , child) {
-  //       return MaterialApp.router(
-  //         theme: ThemeData.light().copyWith(scaffoldBackgroundColor: const Color(0xffE9E9E9)),
-  //     debugShowCheckedModeBanner: false,
-  //     routerConfig: AppRouters.router,
-  //   );
-  // }
-  // );
+    return ScreenUtilInit(
+        designSize: const Size(375, 812),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (_, child) {
+          return MaterialApp.router(
+            theme: ThemeData.light()
+                .copyWith(scaffoldBackgroundColor: const Color(0xffE9E9E9)),
+            debugShowCheckedModeBanner: false,
+            routerConfig: AppRouters.router,
+          );
+        });
   }
 }
